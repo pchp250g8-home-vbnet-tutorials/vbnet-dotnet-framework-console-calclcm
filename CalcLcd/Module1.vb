d57ad944c513a3f179aff183eb065e6d926095ac
Alexander <pchp250g8@tutamail.com>
2025-12-12 15:17:58 +0300

    Добавьте файлы проекта.

---
+++ CalcLcd/Module1.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim iNum1 As Int64, iNum2 As Int64
+        Console.Clear()
+        Console.WriteLine("Input the First Integer Number:")
+        Dim strNum = Console.ReadLine()
+        Int64.TryParse(strNum, iNum1)
+        Console.WriteLine("Input The Second Integer Number:")
+        strNum = Console.ReadLine()
+        Int64.TryParse(strNum, iNum2)
+        Dim iVal1 = Math.Abs(iNum1)
+        Dim iVal2 = Math.Abs(iNum2)
+        If (iVal1 > 0 And iVal2 > 0) Then
+            Dim iVal3 = iVal1
+            Dim iVal4 = iVal2
+            Do While (iVal1 <> iVal2)
+                If (iVal1 > iVal2) Then
+                    iVal1 -= iVal2
+                    iVal3 += iVal4
+                Else
+                    iVal2 -= iVal1
+                    iVal4 += iVal3
+                End If
+            Loop
+            Dim nLcm = (iVal3 + iVal4) / 2
+            Console.WriteLine("The Lcm of the numbers {0} and {1} is {2}", iNum1, iNum2, nLcm)
+        Else
+            Console.WriteLine("The numbers must not be equal 0")
+        End If
+        Console.Read()
+    End Sub
+
+End Module
---
+++ CalcLcd/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("CalcLcd")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CalcLcd")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("0beb1e9a-9361-459c-b1c6-8794800497d9")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
